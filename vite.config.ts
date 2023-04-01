b9f10e8c87ae77c42b998644afed49cf26e826c0
ryu-no-suke <ryunosuke.dev@gmail.com>
2023-04-01 17:17:32 +0900

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      useCredentials: true,
+      manifest: {
+        name: "test app",
+        short_name: "test app",
+        icons: [
+          {
+            src: "maskable_icon_x192.png",
+            sizes: "196x196",
+            type: "image/png",
+            // purpose: "any maskable",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+    }),
+  ],
+});
